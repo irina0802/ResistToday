09038a2cb03e0917fad2f83f2882d56cc1274663
Irina <izoccolini6393@bths.edu>
2018-07-17 16:05:04 -0400

    fix layout

--- client/main.js
+++ client/main.js
@@ -3,20 +3,38 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
+// Template.hello.onCreated(function helloOnCreated() {
+//   // counter starts at 0
+//   this.counter = new ReactiveVar(0);
+// });
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
+// Template.hello.helpers({
+//   counter() {
+//     return Template.instance().counter.get();
+//   },
+// });
+
+// Template.navBar.events({
+//   'click button'(event, instance) {
+//     // increment the counter when button is clicked
+//     location.href='find.html';
+//   },
+// });
 
 Template.navBar.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    location.href='find.html';
-  },
+  'click #myButton': function(event, template) {
+    event.preventDefault();  
+    Router.go('anotherpath');
+  }
 });
+
+Router.map(function() {
+  this.route('anotherpath', {
+    path: 'about.html',
+
+    layoutTemplate: 'myLayout',
+
+    controller: myController
+  });
+});
+
